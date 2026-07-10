bebb4839d77dd32f17ef72948198fee57e75edd6
stefanyroa524-creator <stefanyroa524@gmail.com>
2026-07-10 16:34:31 -0500

    otras operaciones

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 numero1 = int(input())
 numero2 = int(input())
+resultado = numero1 + numero2
+print(resultado)
+
 operacion = input()
 
-if operacion == "suma":
-    resultado = numero1 + numero2
-elif operacion == "resta":
-    resultado = numero1 - numero2
-elif operacion == "multiplicar":
-    resultado = numero1 * numero2
-elif operacion == "Dividir":
-    resultado = numero1 / numero2
-print(resultado)
+if operacion == "resta":
+    print(numero1 - numero2)
+elif operacion == "multiplicacion":
+    print(numero1 * numero2)
+elif operacion == "division":
+    print(numero1 / numero2)
